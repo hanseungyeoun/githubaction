612540d3fc3af3b3a9af230598d41391d08442f2
syhan <aksla1122@naver.com>
2022-11-26 20:04:48 +0900

    fix- build error

--- src/main/java/com/example/githubaction/GithubactionApplication.java
+++ src/main/java/com/example/githubaction/GithubactionApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class GithubactionApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(GithubactionApplication.class, args);
-    }
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)public class GithubactionApplication {
+public static void main(String[] args) {
+    SpringApplication.run(GithubactionApplication.class, args);
+}
 
 }
